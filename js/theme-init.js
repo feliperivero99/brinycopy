1f67db4a65a0c0acfd1411490295801083d9d6a1
Felipe Rivero <firvero@shokworks.ior>
2020-01-02 17:37:01 -0400

    reduce

--- js/theme-init.js
+++ js/theme-init.js
@@ -1827,3 +1827,39 @@ jQuery( document ).ready(
 		}
 	}
 );
+
+
+
+    var elementorFrontendConfig = {
+            "environmentMode": {
+                "edit": false,
+                "wpPreview": false
+            },
+            "is_rtl": false,
+            "breakpoints": {
+                "xs": 0,
+                "sm": 480,
+                "md": 768,
+                "lg": 1025,
+                "xl": 1440,
+                "xxl": 1600
+            },
+            "version": "2.8.2",
+            "urls": {
+                "assets": "http:\/\/briny.ancorathemes.com\/wp-content\/plugins\/elementor\/assets\/"
+            },
+            "settings": {
+                "page": [],
+                "general": {
+                    "elementor_stretched_section_container": ".page_wrap",
+                    "elementor_global_image_lightbox": "yes"
+                },
+                "editorPreferences": []
+            },
+            "post": {
+                "id": 514,
+                "title": "Home 4",
+                "excerpt": ""
+            }
+        };
+    
